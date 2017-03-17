857443f8023ce3e3e1e73c0a9e46559f23040c96
Cayon <cayon.1318.96@hotmail.com>
2017-03-18 04:25:36 +0800

    Avx implementation

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS = -msse2 --std gnu99 -O0 -Wall -Wextra
+CFLAGS = -msse2 --std gnu99 -O0 -Wall -Wextra -mavx2
 
 GIT_HOOKS := .git/hooks/applied
 
@@ -9,11 +9,29 @@ $(GIT_HOOKS):
 	@scripts/install-git-hooks
 	@echo
 
-cache-miss: main
+cache-miss-naive: main
+	$(CC) $(CFLAGS) -DNAIVE -DNAME=\"cache-miss-naive\" -o cache-miss-naive main.c
 	perf stat --repeat 100 \
-		-e cache-misses,cache-references,instructions,cycles \
-		./main
+		-e cache-misses,cache-references,L1-dcache-load-misses,L1-dcache-store-misses,L1-dcache-prefetch-misses,L1-icache-load-misses \
+		./cache-miss-naive
 
+cache-miss-sse: main
+	$(CC) $(CFLAGS) -DSSE -DNAME=\"cache-miss-sse\" -o cache-miss-sse main.c
+	perf stat --repeat 100 \
+		-e cache-misses,cache-references,L1-dcache-load-misses,L1-dcache-store-misses,L1-dcache-prefetch-misses,L1-icache-load-misses \
+		./cache-miss-sse
+
+cache-miss-ssep: main
+	$(CC) $(CFLAGS) -DSSE_P -DNAME=\"cache-miss-ssep\" -o cache-miss-ssep main.c
+	perf stat --repeat 100 \
+		-e cache-misses,cache-references,L1-dcache-load-misses,L1-dcache-store-misses,L1-dcache-prefetch-misses,L1-icache-load-misses \
+		./cache-miss-ssep
+
+cache-miss-avx: main
+	$(CC) $(CFLAGS) -DAVX -DNAME=\"cache-miss-avx\" -o cache-miss-avx main.c
+	perf stat --repeat 100 \
+		-e cache-misses,cache-references,L1-dcache-load-misses,L1-dcache-store-misses,L1-dcache-prefetch-misses,L1-icache-load-misses \
+		./cache-miss-avx
 
 clean:
 	$(RM) main
--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 
 #include <xmmintrin.h>
 
+#include <immintrin.h>
+
 #define TEST_W 4096
 #define TEST_H 4096
 
@@ -31,66 +33,52 @@ static long diff_in_us(struct timespec t1, struct timespec t2)
 
 int main()
 {
-    /* verify the result of 4x4 matrix */
-    {
-        /*
-        int testin[16] = { 0, 1,  2,  3,  4,  5,  6,  7,
-                              8, 9, 10, 11, 12, 13, 14, 15
-                            };
-           int testout[16];
-           int expected[16] = { 0, 4,  8, 12, 1, 5,  9, 13,
-                                2, 6, 10, 14, 3, 7, 11, 15
-                              };
 
-           for (int y = 0; y < 4; y++) {
-               for (int x = 0; x < 4; x++)
-                   printf(" %2d", testin[y * 4 + x]);
-               printf("\n");
-           }
-           printf("\n");
-           sse_transpose(testin, testout, 4, 4);
-           for (int y = 0; y < 4; y++) {
-               for (int x = 0; x < 4; x++)
-                   printf(" %2d", testout[y * 4 + x]);
-               printf("\n");
-           }
-           assert(0 == memcmp(testout, expected, 16 * sizeof(int)) &&
-                  "Verification fails");
-        */
-    }
+    struct timespec start, end;
+    int *src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
 
-    {
-        struct timespec start, end;
-        int *src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out0 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        // int *out1 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        // int *out2 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+    srand(time(NULL));
+    for (int y = 0; y < TEST_H; y++)
+        for (int x = 0; x < TEST_W; x++)
+            *(src + y * TEST_W + x) = rand();
 
-        srand(time(NULL));
-        for (int y = 0; y < TEST_H; y++)
-            for (int x = 0; x < TEST_W; x++)
-                *(src + y * TEST_W + x) = rand();
+#ifdef SSE_P
+    int *out0 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+    clock_gettime(CLOCK_REALTIME, &start);
+    sse_prefetch_transpose(src, out0, TEST_W, TEST_H);
+    clock_gettime(CLOCK_REALTIME, &end);
+    //printf("sse prefetch: \t %ld us\n", diff_in_us(start, end));
+    free(out0);
+#endif
 
-        clock_gettime(CLOCK_REALTIME, &start);
-        sse_prefetch_transpose(src, out0, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse prefetch: \t %ld us\n", diff_in_us(start, end));
+#ifdef SSE
+    int *out1 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+    clock_gettime(CLOCK_REALTIME, &start);
+    sse_transpose(src, out1, TEST_W, TEST_H);
+    clock_gettime(CLOCK_REALTIME, &end);
+    //printf("sse: \t\t %ld us\n", diff_in_us(start, end));
+    free(out1);
+#endif
 
-        /*clock_gettime(CLOCK_REALTIME, &start);
-        sse_transpose(src, out1, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse: \t\t %ld us\n", diff_in_us(start, end));*/
+#ifdef NAIVE
+    int *out2 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+    clock_gettime(CLOCK_REALTIME, &start);
+    naive_transpose(src, out2, TEST_W, TEST_H);
+    clock_gettime(CLOCK_REALTIME, &end);
+    //printf("naive: \t\t %ld us\n", diff_in_us(start, end));
+    free(out2);
+#endif
 
-        /*clock_gettime(CLOCK_REALTIME, &start);
-        naive_transpose(src, out2, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("naive: \t\t %ld us\n", diff_in_us(start, end));*/
+#ifdef AVX
+    int *out3 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+    clock_gettime(CLOCK_REALTIME, &start);
+    avx_transpose(src, out3, TEST_W, TEST_H);
+    clock_gettime(CLOCK_REALTIME, &end);
+    printf("avx: \t\t %ld us\n", diff_in_us(start, end));
+    free(out3);
+#endif
 
-        free(src);
-        free(out0);
-        //free(out1);
-        //free(out2);
-    }
+    free(src);
 
     return 0;
 }
